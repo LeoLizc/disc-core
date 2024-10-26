7a32ccda55800420f9c28ad6957fbda1e53ae5f5
LeoLizc <leolizcanop@gmail.com>
2024-10-26 15:52:58 -0500

    Delete core folder

--- src/core/index.ts
+++
@@ -1,3 +0,0 @@
-export { DiscFactory } from './factory';
-export { Module } from './module';
-export { SlashCommandBuilder } from './slashCBuilder';
--- src/core/types/discFactory.d.ts
+++
@@ -1,6 +0,0 @@
-import { type ClientOptions } from 'discord.js';
-import { Module } from '../module';
-
-export interface DiscFactoryOptions extends ClientOptions {
-  modules?: Module[];
-}
